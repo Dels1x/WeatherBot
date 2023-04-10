df5f01ad699b122a057c1c06f96cad52ac03ba95
delsix <delsixreborn@gmail.com>
2023-04-10 22:32:19 +0300

    simple echo bot

--- src/main/java/ua/delsix/controller/UpdateController.java
+++ src/main/java/ua/delsix/controller/UpdateController.java
@@ -1,4 +1,21 @@
 package ua.delsix.controller;
 
+import org.springframework.stereotype.Controller;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ua.delsix.utils.MessageUtils;
+
+@Controller
 public class UpdateController {
+    private TelegramBot telegramBot;
+
+    public void registrateBot(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    public void processUpdate(Update update) {
+        SendMessage sendMessage = MessageUtils.sendMessageBuilder(update,
+                update.getMessage().getText());
+        telegramBot.sendAnswerMessage(sendMessage);
+    }
 }
